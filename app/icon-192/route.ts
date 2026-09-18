df54ce4a4b366f9f5b8591fdb7415d238cc442c2
patrickfmartins99-spec <patrickfmartins99@gmail.com>
2026-09-18 18:26:51 -0300

    fix: adicionar ícone compatível ao aplicativo

---
+++ app/icon-192/route.ts
@@ -0,0 +1,12 @@
+import { ImageResponse } from 'next/og';
+import { createElement } from 'react';
+import { AppIconArtwork } from '@/components/app-icon-artwork';
+
+export const runtime = 'edge';
+
+export function GET() {
+  return new ImageResponse(createElement(AppIconArtwork, { size: 192 }), {
+    width: 192,
+    height: 192,
+  });
+}
